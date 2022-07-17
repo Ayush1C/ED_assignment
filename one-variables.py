268c065fef06740111160557b2467edad535ba42
Ayush1C <109467844+Ayush1C@users.noreply.github.com>
2022-07-17 18:36:50 +0530

    Add files via upload

---
+++ one-variables.py
@@ -0,0 +1,14 @@
+no = 20
+print(no)
+
+# here no is variable
+# here 20 is value
+# and we have assigned value (20) to the variable (no)
+
+#Rules for defining an Identifier (Variables, Functions, Class, Methods)
+#1. It can only include A-Z, a-z, 0-9, _
+#2. It should not start with a digit
+#3. $ is not allowed in Python.
+#4. No spaces allowed
+#5. It is case sensitive
+#6. No keywords allowed
---
+++ two-datatypes.py
@@ -0,0 +1,67 @@
+'''
+Datatypes
+1. int     - Whole number
+2. float   - decimal values
+3. str     - 
+4. bool    -
+5. complex -
+6. list    -
+7. tuple   -
+8. set     -
+9. dict    -
+10. None   -
+'''
+
+#int
+#num = -20
+#print(type(num))
+
+#float
+#num = 67.5
+#print(type(num))
+
+#str
+#name= "Bharti"
+
+#bool
+#human = True
+
+#complex
+#comp = 7+9j
+
+#list
+#[]
+#it stores heterogenous data
+# It can add Duplicate data
+#indexed based
+# Muttable(modifiable)
+#doc = [5,6,7,"Bharti",'A',True]
+#print(type(doc))
+
+#tuple
+#()
+#can store heterogenous data
+# Duplicate data allowed
+# indexed based
+# Immutable
+#num = (4,5,3,"LOW")
+#print(type(num))
+#print(num[0])
+
+#set
+#{}
+#
+num = {1,2,3,4.5,3,True,"Task"}
+print(num)
+print(type(num))
+print(num[3])
+
+#dict
+#dict_demo = {"one":"Bharti","two":"Varun"}
+#print(dict_demo)
+#print(type(dict_demo))
+
+#None 
+#language = None
+#print(language)
+#print(type(language))
